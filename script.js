fb34a5857ce3bf180d5b756a207fb588d66f2db0
Gonzalo Padilla <107971962+gonza2323@users.noreply.github.com>
2025-03-07 15:07:44 -0300

    Fix read status bug

--- script.js
+++ script.js
@@ -13,9 +13,16 @@ function Book(title, author, noPages, read) {
 }
 
 
-function addBook(title, author, noPages, read) {
-    const book = new Book(title, author, noPages, read);
-    library.push(book);
+Book.prototype.toggleReadStatus = function() {
+    this.isRead = !this.isRead;
+}
+
+
+// ----------------------------------------------------------------------------
+
+function addBook(title, author, noPages, isRead) {
+    const book = new Book(title, author, noPages, isRead);
+    library.unshift(book);
     return book;
 }
 
@@ -28,24 +35,25 @@ function deleteBook(id) {
     }
 }
 
-function updateBookRead(id, read) {
+
+function toggleBookReadStatus(id) {
     const book = library.find(book => book.id === id);
     if (book)
-        book.read = read;
+        book.toggleReadStatus();
 }
 
 
 // UI Logic
 
-function displayBooks() {
-    const booksContainer = document.querySelector(".books");
-    const oldBooks = booksContainer.querySelectorAll(".book");
+function drawBookCards() {
+    const cardsContainer = document.querySelector(".books");
+    const oldCards = cardsContainer.querySelectorAll(".book");
     
-    oldBooks.forEach(e => e.remove());
+    oldCards.forEach(e => e.remove());
     
     for (const book of library) {
         const bookCard = createBookCard(book);
-        booksContainer.appendChild(bookCard);
+        cardsContainer.appendChild(bookCard);
     }
 }
 
@@ -68,10 +76,9 @@ function createBookCard(book) {
     readCheck.checked = book.isRead;
 
     card.classList.remove("template");
-    card.removeAttribute("hidden");
 
     const deleteButton = card.querySelector(".delete");
-    deleteButton.addEventListener("click", () => deleteCard(book.id));
+    deleteButton.addEventListener("click", () => deleteBookCard(book.id));
     
     const readButton = card.querySelector(".read");
     readButton.addEventListener("input",
@@ -90,25 +97,25 @@ function addBookCard(event) {
     
     const book = addBook(title, author, noPages, false);
     const card = createBookCard(book);
-    const booksContainer = document.querySelector(".books");
-    const firstBook = booksContainer.querySelector(".book");
+    
+    const cardsContainer = document.querySelector(".books");
+    const firstBookCard = cardsContainer.querySelector(".book");
 
-    booksContainer.insertBefore(card, firstBook);    
+    cardsContainer.insertBefore(card, firstBookCard);    
     form.reset();
 }
 
 
-function deleteCard(id) {
+function deleteBookCard(id) {
     deleteBook(id);
     const card = document.querySelector(`.book[uuid="${id}"]`);
-    if (card) {
+    if (card)
         card.remove();
-    }
 }
 
 
 function changeReadStatus(id, readText, checkbox) {
-    updateBookRead(id, checkbox.checked);
+    toggleBookReadStatus(id, checkbox.checked);
     readText.textContent = checkbox.checked ? "Read" : "Unread";
 }
 
@@ -125,11 +132,11 @@ addBookButton.addEventListener("click", () => {
 addBookDialog.addEventListener("submit", addBookCard);
 
 
-// test
+// initial data
 
 addBook("1984", "George Orwell", "400", false);
 addBook("The Hobbit", "J.R.R Tolkien", "800", true);
 addBook("The Time Machine", "H.G. Wells", "500", true);
 addBook("Dune", "Frank Herbert", "800", false);
 
-displayBooks();
+drawBookCards();
